1a64283879288bc6022b48efd1ef15f48d56f50c
RomanBeldent <romanbeldent@gmail.com>
2025-06-11 19:35:06 +0200

    feat: permet de mettre à jour le statut avec une requête http PATCH

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -26,6 +26,15 @@ export class TasksService {
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task | undefined {
+    const task = this.getTaskById(id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    task.status = status;
     return task;
   }
 
